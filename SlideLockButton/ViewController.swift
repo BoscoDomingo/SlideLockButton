bbaff11cbe659cad62f470a64d0234218aa6ac80
Bosco Domingo <boscodomingob@gmail.com>
2020-02-19 18:48:38 +0100

    Added new example

---
+++ SlideLockButton/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SlideLockButton
+//
+//  Created by Bosco Domingo on 19/02/2020.
+//  Copyright © 2020 Bosco Domingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SlideLockButtonDelegate {
+    @IBOutlet weak var slideButton: SlideLockButton!
+    
+    @IBAction func resetPressed(_ sender: UIButton) {
+        slideButton.reset()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slideButton.delegate = self //setting the delegate
+    }
+    
+    //Delegate method
+    func buttonStatus(status: SlideLockButton.Status, sender: SlideLockButton) {
+        print(status.rawValue)
+    }
+}
+
